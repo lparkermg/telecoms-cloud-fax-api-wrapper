b61d4ba49f9019fef24aad8d30f3f2e26c34abb6
Luke Parker <luke.parker@bathasu.com>
2015-07-02 16:19:15 +0100

    Built a test project.

---
+++ src/Fax-Wrapper-Test/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API;
+using API.Auth;
+
+namespace Fax_Wrapper_Test
+{
+    class Program
+    {
+        private const string _clientId = "";
+        private const string _clientSecret = "";
+        private const string _baseUrl = "https://api.telecomscloud.com/v1/";
+        private static OAuth _oAuth = new OAuth(_clientId,_clientSecret,_baseUrl);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Fax Test:\n\n");
+
+            Console.Write("Asking for Access to API: ");
+
+            var clientResp = _oAuth.GetAccess();
+
+            Console.Write("[Completed]\n\n");
+
+            Console.Write("Getting Test Fax Collection: ");
+
+            var faxInbound = new FaxInbound(clientResp,_baseUrl);
+
+            var faxCollection = faxInbound.GetFaxCollection(0);
+
+            Console.Write("[Complete]\n\n");
+
+            Console.Write("Downloading Single Fax to Application Location: ");
+
+            faxInbound.DownloadFax(faxCollection.Records[0].Id,"DownloadedFax.tiff");
+
+            Console.Write("[Complete]\n\n");
+
+            Console.Write("All tests completed, press any key to close.");
+
+            Console.ReadLine();
+
+        }
+    }
+}
