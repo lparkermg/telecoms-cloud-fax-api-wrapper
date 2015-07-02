19b326121942065d9af26fbddc4323934f9b4e3f
Luke Parker <luke.parker@bathasu.com>
2015-07-02 09:43:45 +0100

    Setup Project.

---
+++ src/API/Entities/ClientGrantResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class ClientGrantResponse
+    {
+        public string AccessToken { get; private set; }
+        private string _accessToken;
+
+        public int ExpiresIn { get; private set; }
+        private int _expiresIn;
+
+        public string TokenType { get; private set; }
+        private string _tokenType;
+
+        public string Scope { get; private set; }
+        private string _scope;
+
+    }
+}
