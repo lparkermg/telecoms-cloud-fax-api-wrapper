fbff7a5115e2abd1253b956438a83124425ce65d
Luke Parker <luke.parker@bathasu.com>
2015-07-02 11:04:07 +0100

    Added ClientGrantResponse.

--- src/API/Entities/ClientGrantResponse.cs
+++ src/API/Entities/ClientGrantResponse.cs
@@ -8,17 +8,25 @@ namespace API.Entities
 {
     public class ClientGrantResponse
     {
-        public string AccessToken { get; private set; }
+        public string AccessToken { get { return _accessToken; } }
         private string _accessToken;
 
-        public int ExpiresIn { get; private set; }
+        public int ExpiresIn { get { return _expiresIn; } }
         private int _expiresIn;
 
-        public string TokenType { get; private set; }
+        public string TokenType { get { return _tokenType; } }
         private string _tokenType;
 
-        public string Scope { get; private set; }
+        public string Scope { get { return _scope; } }
         private string _scope;
+
+        public ClientGrantResponse(string accessToken, int expiresIn, string tokenType, string scope)
+        {
+            _accessToken = accessToken;
+            _expiresIn = expiresIn;
+            _tokenType = tokenType;
+            _scope = scope;
+        }
 
     }
 }
