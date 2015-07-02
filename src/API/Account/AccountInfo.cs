06f7c8645307fb50487dcc8e3961f9db045baf94
Luke Parker <luke.parker@bathasu.com>
2015-07-02 13:20:06 +0100

    Added a bit to AccountInfo.

--- src/API/Account/AccountInfo.cs
+++ src/API/Account/AccountInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,19 @@ namespace API.Account
     public class AccountInfo
     {
         private ClientGrantResponse _clientGrant;
+        private string _baseApiUrl;
 
-        public AccountInfo(ClientGrantResponse clientGrant)
+        public AccountInfo(ClientGrantResponse clientGrant, string baseUrl)
         {
             _clientGrant = clientGrant;
+            _baseApiUrl = baseUrl;
+        }
+
+        public AccountInfoResponse GetAccountInfo()
+        {
+            var completeUrl = _baseApiUrl + "/account/info";
+
+            return new AccountInfoResponse("","",0,0.0f,"");
         }
 
 
