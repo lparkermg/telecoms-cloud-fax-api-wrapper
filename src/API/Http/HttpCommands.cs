87b69887f46e664775cc70745f013b7404f40ca2
Luke Parker <luke.parker@bathasu.com>
2015-07-02 13:48:59 +0100

    Added Post Get and Download via redirect.

--- src/API/Http/HttpCommands.cs
+++ src/API/Http/HttpCommands.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,43 @@ namespace API.Http
             }
         }
 
+        public string HttpGet(string uri, string body, Dictionary<string, string> headers)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var req = new HttpRequestMessage() { RequestUri = new Uri(uri), Method = HttpMethod.Get};
 
+                foreach(var header in headers)
+                    req.Headers.Add(header.Key,header.Value);
+
+                var resp = client.SendAsync(req).Result;
+
+                return resp.Content.ToString();
+            }
+        }
+
+        public void DownloadViaRedirect(string uri, string destinationPath, Dictionary<string, string> headers)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var req = new HttpRequestMessage() {RequestUri = new Uri(uri), Method=HttpMethod.Get};
+
+                foreach(var header in headers)
+                    req.Headers.Add(header.Key,header.Value);
+
+                var resp = client.SendAsync(req).Result;
+
+                var redirect = req.RequestUri;
+
+                using (var fileData = client.GetStreamAsync(redirect).Result)
+                {
+                    using (var fs = new FileStream(destinationPath, FileMode.Create))
+                    {
+                        fileData.CopyTo(fs);
+                        fs.Flush();
+                    }
+                }
+            }
+        }
     }
 }
